ac966cd641361871f621236297ac09fe007a8bbf
kimbochen <github@thc.anonaddy.com>
2022-02-03 15:39:36 +0800

    Completed error handling.

--- parsing.c
+++ parsing.c
@@ -6,40 +6,104 @@
 #include "mpc/mpc.h"
 
 
-long eval_op(char *op, long x, long y)
+typedef struct
 {
-    switch (op[0]) {
-        case '+':
-            return x + y;
-        case '-':
-            return x - y;
-        case '*':
-            return x * y;
-        case '/':
-            return x / y;
+    int type;
+    long num;
+    int err;
+} lval;
+
+enum { LVAL_NUM, LVAL_ERR };
+enum { LERR_DIV_ZERO, LERR_BAD_OP, LERR_BAD_NUM };
+
+
+lval create_lval_num(long num_)
+{
+    lval v;
+    v.type = LVAL_NUM;
+    v.num = num_;
+    return v;
+}
+
+lval create_lval_err(int err_)
+{
+    lval v;
+    v.type = LVAL_ERR;
+    v.err = err_;
+    return v;
+}
+
+
+void lval_print(lval v)
+{
+    switch (v.type) {
+        case LVAL_NUM:
+            printf("%li\n", v.num);
+            break;
+        case LVAL_ERR:
+            switch (v.err) {
+                case LERR_DIV_ZERO:
+                    puts("Error: Division by zero.");
+                    break;
+                case LERR_BAD_OP:
+                    puts("Error: Invalid operator.");
+                    break;
+                case LERR_BAD_NUM:
+                    puts("Error: Invalid number.");
+                    break;
+                default:
+                    puts("Error: Invalid error type.");
+            }
+            break;
         default:
-            return -1;
+            puts("Error: Invalid Lisp Value.");
     }
 }
 
-long eval(mpc_ast_t *node)
+
+lval eval_op(char *op, lval lval_x, lval lval_y)
+{
+    if (lval_x.type == LVAL_ERR) return lval_x;
+    if (lval_y.type == LVAL_ERR) return lval_y;
+
+    long x = lval_x.num;
+    long y = lval_y.num;
+
+    switch (op[0]) {
+        case '+':
+            return create_lval_num(x + y);
+        case '-':
+            return create_lval_num(x - y);
+        case '*':
+            return create_lval_num(x * y);
+        case '/':
+            return (y == 0) ? create_lval_err(LERR_DIV_ZERO) : create_lval_num(x / y);
+        default:
+            return create_lval_err(LERR_BAD_OP);
+    }
+}
+
+lval eval(mpc_ast_t *node)
 {
     // If node is not a number
     if (strstr(node->tag, "number") == NULL) {
         mpc_ast_t **children = node->children;
 
         char *op = children[1]->contents;
-        long x = eval(children[2]);
+        lval x = eval(children[2]);
 
         for (int i = 3; strstr(children[i]->tag, "expr"); i++)
         {
-            long y = eval(children[i]);
+            lval y = eval(children[i]);
             x = eval_op(op, x, y);
         }
         return x;
     }
     else { // node is a number, return the number
-        return atoi(node->contents);
+        errno = 0;
+        long x = strtol(node->contents, NULL, 10);
+        return (errno == ERANGE) ?
+            create_lval_err(LERR_BAD_NUM) : create_lval_num(x);
     }
 }
 
@@ -78,8 +142,8 @@ int main(int argc, char **argv)
         mpc_result_t r;
 
         if (mpc_parse("<stdin>", input, Lispy, &r)) {
-            long result = eval(r.output);
-            printf("%li\n", result);
+            lval result = eval(r.output);
+            lval_print(result);
             mpc_ast_delete(r.output);
         }
         else {
